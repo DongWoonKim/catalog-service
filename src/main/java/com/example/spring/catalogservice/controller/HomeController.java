e973fa2ec0fcf498bf9aaa0050c5a7cb68279cb4
DongWoonKim <01030985843z@naver.com>
2024-10-16 09:39:20 +0900

    springboot catalog-service configuration

--- src/main/java/com/example/spring/catalogservice/controller/HomeController.java
+++ src/main/java/com/example/spring/catalogservice/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.example.spring.catalogservice.controller;
 
+import com.example.spring.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final PolarProperties polarProperties;
 
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카탈로그에 오신 것을 환영합니다.";
+        return polarProperties.getGreeting();
     }
 
 }
