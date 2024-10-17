53a7b39d1c1020ceac6d7bef812cd08831771135
DongWoonKim <01030985843z@naver.com>
2024-10-17 11:18:51 +0900

    catalog service set db test code

--- src/test/java/com/example/spring/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/example/spring/catalogservice/CatalogServiceApplicationTests.java
@@ -4,17 +4,38 @@ import com.example.spring.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@Testcontainers
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @Container
+    private static final MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.33")
+            .withDatabaseName("test")
+            .withUsername("root")
+            .withPassword("1234");
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mysqlContainer::getUsername);
+        registry.add("spring.datasource.password", mysqlContainer::getPassword);
+    }
 
     @Test
     void whenPostRequestThenBookCreated() {
